1ede1e083d7dd21430f6dc7d3fac473cfe6a7e1f
Ana Ursachi <aniuta200020@gmail.com>
2021-07-02 23:57:04 +0300

    second commit

---
+++ Desktop/JS_first_exercice.js
@@ -0,0 +1,8 @@
+const kelvin = 293;
+/*The forecast today is 293 Kelvin. To start, create a variable named kelvin, and set it equal to 293.
+The value saved to kelvin will stay constant. Choose the variable type with this in mind.*/
+var celsius = kelvin - 273;
+var fahrenheit = celsius * (9 / 5) + 32;
+// calculate Fahrenheit//
+console.log('The temperature is' + ' ' + Math.floor(fahrenheit) + ' ' + 'degrees Fahrenheit');
+//in console will be write value of temperature in Fahrenheit//
