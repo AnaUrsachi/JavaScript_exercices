d18f9e26d07995623ae509b5712556b8e16b31cf
Ana Ursachi <aniuta200020@gmail.com>
2021-07-03 01:02:22 +0300

    third commit

---
+++ Desktop/JS/JS_second_ex.js
@@ -0,0 +1,15 @@
+var myAge = 21;
+//it is var that have value my age in numbers//
+var earlyYears = 2;
+//the value saved to this variable will change//
+earlyYears *= 10.5;
+//We’ll be changing this value later.//
+var laterYears = myAge - 2;
+laterYears *= 4;
+//calculate the number of dog years accounted for by my later years//
+var myAgeInDogYears = earlyYears + laterYears;
+//Add earlyYears and laterYears together and will calculate my age in dog years//
+var myName = "Ana".toUpperCase();
+//The toLowerCase method returns a string with all lowercase letters.//
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+//My name is NAME. I am HUMAN AGE years old in human years which is DOG AGE years old in dog years.//
