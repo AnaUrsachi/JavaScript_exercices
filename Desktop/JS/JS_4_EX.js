575f407288937c6613006a2406ef174d7d4f70a3
Ana Ursachi <aniuta200020@gmail.com>
2021-07-03 22:22:54 +0300

    4ex

---
+++ Desktop/JS/JS_4_EX.js
@@ -0,0 +1,12 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+let RegistredEarly = 9.30;
+let RegistredLater = 11.00;
+let runnerAge = Number;
+if (runnerAge >= '18' && RegistredEarly < '9.31') {
+    console.log(raceNumber + 1000);
+} else if (runnerAge >= '18' && RegistredLater < '11.01') {
+    console.log(raceNumber + 1000);
+};
+if (runnerAge < '18' && RegistredEarly < '12.30') {
+    console.log(raceNumber);
+};
